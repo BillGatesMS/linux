4b9a9fbca1695242cbc54a01081f210611d0e929
Kever Yang <kever.yang@rock-chips.com>
2017-06-28 20:35:43 -0700

    enable codec
    Signed-off-by: Kever Yang <kever.yang@rock-chips.com>

--- include/linux/clk-provider.h
+++ include/linux/clk-provider.h
@@ -517,6 +517,7 @@ struct clk_fractional_divider {
 };
 
 extern const struct clk_ops clk_fractional_divider_ops;
+extern const struct clk_ops clk_fractional_divider_special_ops;
 struct clk *clk_register_fractional_divider(struct device *dev,
 		const char *name, const char *parent_name, unsigned long flags,
 		void __iomem *reg, u8 mshift, u8 mwidth, u8 nshift, u8 nwidth,
